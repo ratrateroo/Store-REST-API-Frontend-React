0b243fcf50e43e2de3cd027c1d5c431809c29471
Mark <ratrateroo@gmail.com>
2020-02-27 02:10:23 +0800

    initial commit

---
+++ store-rest-api-frontend-react/src/App.js
@@ -0,0 +1,8 @@
+import React, { Component, Fragment } from "react";
+import { Route, Switch, Redirect, withRouter } from "react-router-dom";
+
+import "./App.css";
+
+class App extends Component {}
+
+export default withRouter(App);
