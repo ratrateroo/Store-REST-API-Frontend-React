720da6a6518e453c853620fd96fe8b9893ec86c2
Mark <ratrateroo@gmail.com>
2020-02-27 03:39:02 +0800

    added header and logo

--- store-rest-api-frontend-react/src/App.js
+++ store-rest-api-frontend-react/src/App.js
@@ -1,8 +1,30 @@
 import React, { Component, Fragment } from "react";
 import { Route, Switch, Redirect, withRouter } from "react-router-dom";
 
+import Header from "./components/Header/Header";
 import "./App.css";
 
-class App extends Component {}
+class App extends React.Component {
+  componentDidMount() {
+    window.addEventListener("scroll", this.handleScrollToElement);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("scroll", this.handleScrollToElement);
+  }
+
+  handleScrollToElement(event) {
+    console.log("Fired " + event);
+    const header = document.querySelector(".header");
+    header.classList.toggle("sticky", window.scrollY > 0);
+  }
+  render() {
+    return (
+      <Fragment>
+        <Header></Header>
+      </Fragment>
+    );
+  }
+}
 
 export default withRouter(App);
---
+++ store-rest-api-frontend-react/src/components/Header/Header.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+import Logo from "../Logo/Logo";
+import "./Header.css";
+
+const header = props => (
+  <header className="header">
+    <nav className="navigation">
+      <div className="navigation__logo">
+        <Logo />
+      </div>
+      <div className="navigation__items">
+        <ul className="navigation__links">
+          <li className="navigation__link">
+            <a href="products.html">Products</a>
+          </li>
+          <li className="navigation__link">
+            <a href="myproducts.html">My Products</a>
+          </li>
+          <li className="navigation__link">
+            <a href="add-product.html">Add Product</a>
+          </li>
+        </ul>
+      </div>
+      <div className="navigation__access">
+        <ul className="navigation__links">
+          <li className="navigation__link">
+            <a href="login.html">Login</a>
+          </li>
+          <li className="navigation__link">
+            <a href="signup.html">Signup</a>
+          </li>
+          <li className="navigation__link">
+            <a href="logout.html">Logout</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </header>
+);
+
+export default header;
