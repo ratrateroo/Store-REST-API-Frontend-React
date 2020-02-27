28c098638f8dee4b33239472deded34c2981f5e7
Mark <ratrateroo@gmail.com>
2020-02-27 23:25:35 +0800

    added modal

---
+++ store-rest-api-frontend-react/src/components/Modal/Modal.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./Modal.css";
+
+const Modal = props => (
+  <div class="modal">
+    <header class="modal__header">
+      <h1>Modal Title</h1>
+    </header>
+    <div class="modal__conent">Modal Content</div>
+    <div class="modal__actions">
+      <button class="button">Cancel</button>
+      <button class="button">Accept</button>
+    </div>
+  </div>
+);
+
+export default Modal;
