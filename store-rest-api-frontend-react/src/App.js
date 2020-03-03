46570086c4010733f1037bd99f30506830a049a4
Mark <ratrateroo@gmail.com>
2020-03-03 22:14:22 +0800

    set up user signup

--- store-rest-api-frontend-react/src/App.js
+++ store-rest-api-frontend-react/src/App.js
@@ -144,43 +144,45 @@ class App extends Component {
 
   signupHandler = (event, authData) => {
     event.preventDefault();
-    // this.setState({ authLoading: true });
-    // fetch('https://messages-rest-api-backend-node.herokuapp.com/auth/signup', {
-    //   method: 'PUT',
-    //   headers: {
-    //     'Content-Type': 'application/json'
-    //   },
-    //   body: JSON.stringify({
-    //     email: authData.signupForm.email.value,
-    //     password: authData.signupForm.password.value,
-    //     name: authData.signupForm.name.value
-    //   })
-    // })
-    //   .then(res => {
-    //     if (res.status === 422) {
-    //       throw new Error(
-    //         "Validation failed. Make sure the email address isn't used yet!"
-    //       );
-    //     }
-    //     if (res.status !== 200 && res.status !== 201) {
-    //       console.log('Error!');
-    //       throw new Error('Creating a user failed!');
-    //     }
-    //     return res.json();
-    //   })
-    //   .then(resData => {
-    //     console.log(resData);
-    //     this.setState({ isAuth: false, authLoading: false });
-    //     this.props.history.replace('/');
-    //   })
-    //   .catch(err => {
-    //     console.log(err);
-    //     this.setState({
-    //       isAuth: false,
-    //       authLoading: false,
-    //       error: err
-    //     });
-    //   });
+    this.setState({ authLoading: true });
+    fetch("http://localhost:8080/auth/signup", {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        email: authData.signupForm.email.value,
+        password: authData.signupForm.password.value,
+        firstname: authData.signupForm.firstname.value,
+        middlename: authData.signupForm.middlename.value,
+        lastname: authData.signupForm.lastname.value
+      })
+    })
+      .then(res => {
+        if (res.status === 422) {
+          throw new Error(
+            "Validation failed. Make sure the email address isn't used yet!"
+          );
+        }
+        if (res.status !== 200 && res.status !== 201) {
+          console.log("Error!");
+          throw new Error("Creating a user failed!");
+        }
+        return res.json();
+      })
+      .then(resData => {
+        console.log(resData);
+        this.setState({ isAuth: false, authLoading: false });
+        this.props.history.replace("/");
+      })
+      .catch(err => {
+        console.log(err);
+        this.setState({
+          isAuth: false,
+          authLoading: false,
+          error: err
+        });
+      });
   };
 
   render() {
