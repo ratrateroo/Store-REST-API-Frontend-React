1b00eed8f8ae174aa49a7a921a7f2d608a9d1ae1
Mark <ratrateroo@gmail.com>
2020-03-04 20:32:26 +0800

    fixed some bugs

--- store-rest-api-frontend-react/src/pages/Feed/Feed.js
+++ store-rest-api-frontend-react/src/pages/Feed/Feed.js
@@ -53,6 +53,22 @@ class Feed extends Component {
     // });
   }
 
+  addPost = post => {
+    this.setState(prevState => {
+      const updatedPosts = [...prevState.posts];
+      if (prevState.postPage === 1) {
+        if (prevState.posts.length >= 2) {
+          updatedPosts.pop();
+        }
+        updatedPosts.unshift(post);
+      }
+      return {
+        posts: updatedPosts,
+        totalPosts: prevState.totalPosts + 1
+      };
+    });
+  };
+
   loadPosts = direction => {
     if (direction) {
       this.setState({ postsLoading: true, posts: [] });
@@ -90,22 +106,6 @@ class Feed extends Component {
         });
       })
       .catch(this.catchError);
-  };
-
-  addPost = post => {
-    this.setState(prevState => {
-      const updatedPosts = [...prevState.posts];
-      if (prevState.postPage === 1) {
-        if (prevState.posts.length >= 2) {
-          updatedPosts.pop();
-        }
-        updatedPosts.unshift(post);
-      }
-      return {
-        posts: updatedPosts,
-        totalPosts: prevState.totalPosts + 1
-      };
-    });
   };
 
   updatePost = post => {
